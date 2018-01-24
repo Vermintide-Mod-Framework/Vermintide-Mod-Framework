4cc3a491531119856d7036a9b5f64886f749e6a5
Azumgi <gkadet@gmail.com>
2018-01-24 18:01:27 +0300

    dev console fix (duplicating strings after reloading)

--- vmf_source/scripts/mods/vmf/modules/dev_console.lua
+++ vmf_source/scripts/mods/vmf/modules/dev_console.lua
@@ -1,21 +1,22 @@
 DEV_CONSOLE_ENABLED = DEV_CONSOLE_ENABLED or false
 
-local print_original_function = print
-
-local print_hook_function = function(func, ...)
-  if DEV_CONSOLE_ENABLED then
-    CommandWindow.print(...)
-    func(...)
-  else
-    func(...)
-  end
-end
-
-print = function(...)
-  print_hook_function(print_original_function, ...)
-end
-
 if DEV_CONSOLE_ENABLED == false then
+
+  local print_original_function = print
+
+  local print_hook_function = function(func, ...)
+    if DEV_CONSOLE_ENABLED then
+      CommandWindow.print(...)
+      func(...)
+    else
+      func(...)
+    end
+  end
+
+  print = function(...)
+    print_hook_function(print_original_function, ...)
+  end
+
   CommandWindow.open("Developer console")
   DEV_CONSOLE_ENABLED = true
 end
