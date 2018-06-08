f6c62a949a8a7ca153fb9f2b59203b737eb98b39
bi <gkadet@gmail.com>
2018-06-08 12:03:23 +0300

    Mutators: little style changes

--- vmf/scripts/mods/vmf/modules/core/mutators/mutators_dice.lua
+++ vmf/scripts/mods/vmf/modules/core/mutators/mutators_dice.lua
@@ -4,40 +4,40 @@
 local vmf = get_mod("VMF")
 
 -- List of all die types
-local missions = {
+local MISSIONS = {
   "bonus_dice_hidden_mission",
   "tome_bonus_mission",
   "grimoire_hidden_mission"
 }
 
 -- Amounts of additional dice to be added at level completion
-local num_dice_per_mission = {
+local _num_dice_per_mission = {
   bonus_dice_hidden_mission = 0,
   tome_bonus_mission = 0,
   grimoire_hidden_mission = 0
 }
 
--- ####################################################################################################################
--- ##### Local functions ##############################################################################################
--- ####################################################################################################################
+-- #####################################################################################################################
+-- ##### Local functions ###############################################################################################
+-- #####################################################################################################################
 
 -- Adds/remove dice
 local function adjustDice(grims, tomes, bonus, multiplier)
   if grims then
-    num_dice_per_mission.grimoire_hidden_mission = num_dice_per_mission.grimoire_hidden_mission + grims * multiplier
+    _num_dice_per_mission.grimoire_hidden_mission = _num_dice_per_mission.grimoire_hidden_mission + grims * multiplier
   end
   if tomes then
-    num_dice_per_mission.tome_bonus_mission = num_dice_per_mission.tome_bonus_mission + tomes * multiplier
+    _num_dice_per_mission.tome_bonus_mission = _num_dice_per_mission.tome_bonus_mission + tomes * multiplier
   end
   if bonus then
-    num_dice_per_mission.bonus_dice_hidden_mission = num_dice_per_mission.bonus_dice_hidden_mission + bonus *
-                                                                                                       multiplier
+    _num_dice_per_mission.bonus_dice_hidden_mission = _num_dice_per_mission.bonus_dice_hidden_mission + bonus *
+                                                                                                         multiplier
   end
 end
 
--- ####################################################################################################################
--- ##### Hooks ########################################################################################################
--- ####################################################################################################################
+-- #####################################################################################################################
+-- ##### Hooks #########################################################################################################
+-- #####################################################################################################################
 
 vmf:hook("GameModeManager.complete_level", function(func, ...)
   local num_dice = 0
@@ -46,8 +46,8 @@ vmf:hook("GameModeManager.complete_level", function(func, ...)
   local active_mission = mission_system.active_missions
 
   -- Add additional dice
-  for _, mission in ipairs(missions) do
-    for _ = 1, num_dice_per_mission[mission] do
+  for _, mission in ipairs(MISSIONS) do
+    for _ = 1, _num_dice_per_mission[mission] do
       mission_system:request_mission(mission, nil, Network.peer_id())
       mission_system:update_mission(mission, true, nil, Network.peer_id(), nil, true)
     end
@@ -55,13 +55,13 @@ vmf:hook("GameModeManager.complete_level", function(func, ...)
 
   -- Get total number of dice
   for name, obj in pairs(active_mission) do
-    if table.contains(missions, name) then
+    if table.contains(MISSIONS, name) then
       num_dice = num_dice + obj.current_amount
     end
   end
 
   -- Remove excess dice
-  for _, mission in ipairs(missions) do
+  for _, mission in ipairs(MISSIONS) do
     if active_mission[mission] then
       for _ = 1, active_mission[mission].current_amount do
         if num_dice > max_dice then
@@ -77,9 +77,9 @@ vmf:hook("GameModeManager.complete_level", function(func, ...)
   func(...)
 end)
 
--- ####################################################################################################################
--- ##### Return #######################################################################################################
--- ####################################################################################################################
+-- #####################################################################################################################
+-- ##### Return ########################################################################################################
+-- #####################################################################################################################
 
 return {
   addDice = function(dice)
