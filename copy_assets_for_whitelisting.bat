64d43f4725787f9374b7a0b3348eba5b22dcc667
bi <gkadet@gmail.com>
2018-07-16 18:44:54 +0300

    Fix wrong folder name inside new batch-file

--- copy_assets_for_whitelisting.bat
+++ copy_assets_for_whitelisting.bat
@@ -1,8 +1,8 @@
 cd vmf
-xcopy /s/y .\core              .\bundleV2\scripts\core\
-xcopy /s/y .\gui               .\bundleV2\scripts\gui\
-xcopy /s/y .\localization      .\bundleV2\scripts\localization\
-xcopy /s/y .\materials         .\bundleV2\scripts\materials\
-xcopy /s/y .\resource_packages .\bundleV2\scripts\resource_packages\
-xcopy /s/y .\scripts           .\bundleV2\scripts\scripts\
-xcopy   /y .\vmf.mod           .\bundleV2\scripts\vmf.mod*
+xcopy /s/y .\core              .\bundleV2\source\core\
+xcopy /s/y .\gui               .\bundleV2\source\gui\
+xcopy /s/y .\localization      .\bundleV2\source\localization\
+xcopy /s/y .\materials         .\bundleV2\source\materials\
+xcopy /s/y .\resource_packages .\bundleV2\source\resource_packages\
+xcopy /s/y .\scripts           .\bundleV2\source\scripts\
+xcopy   /y .\vmf.mod           .\bundleV2\source\vmf.mod*
