9c839f4dd71e9234b1dd7fc844af750d64c677d2
bi <gkadet@gmail.com>
2018-07-16 18:32:36 +0300

    Add batch-file for workshop whitelisting process
    It copies nessessary game assets to "workshop_uploader_folder/scripts",
    so Fatshark will be able to complile VMF themselves and sign it.

---
+++ copy_assets_for_whitelisting.bat
@@ -0,0 +1,8 @@
+cd vmf
+xcopy /s/y .\core              .\bundleV2\scripts\core\
+xcopy /s/y .\gui               .\bundleV2\scripts\gui\
+xcopy /s/y .\localization      .\bundleV2\scripts\localization\
+xcopy /s/y .\materials         .\bundleV2\scripts\materials\
+xcopy /s/y .\resource_packages .\bundleV2\scripts\resource_packages\
+xcopy /s/y .\scripts           .\bundleV2\scripts\scripts\
+xcopy   /y .\vmf.mod           .\bundleV2\scripts\vmf.mod*
