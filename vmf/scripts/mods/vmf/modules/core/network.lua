964e6f6e6ff75b19107778c12a5c247c052d9c72
Azumgi <4zumgi@gmail.com>
2018-10-10 12:09:41 +0300

    Add VT2 1.2.1 Beta compatibility

--- vmf/scripts/mods/vmf/modules/core/network.lua
+++ vmf/scripts/mods/vmf/modules/core/network.lua
@@ -136,8 +136,11 @@ local function rpc_chat_message(member, channel_id, message_sender, message, loc
   if VT1 then
     RPC.rpc_chat_message(member, channel_id, message_sender, message, localization_param,
                           is_system_message, pop_chat, is_dev)
-  else
+  elseif tonumber(script_data.settings.content_revision) <= 122701 then
     RPC.rpc_chat_message(member, channel_id, message_sender, 0, message, localization_param,
+                          is_system_message, pop_chat, is_dev)
+  else
+    RPC.rpc_chat_message(member, channel_id, message_sender, 0, message, {}, false, false,
                           is_system_message, pop_chat, is_dev)
   end
 end
