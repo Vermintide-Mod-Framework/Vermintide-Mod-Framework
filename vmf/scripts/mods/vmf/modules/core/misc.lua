e70b8b5fa5764c33459c18729ccc2d6401553b7e
Azumgi <4zumgi@gmail.com>
2018-10-11 12:49:54 +0300

    Check for old VMF mods

--- vmf/scripts/mods/vmf/modules/core/misc.lua
+++ vmf/scripts/mods/vmf/modules/core/misc.lua
@@ -19,6 +19,15 @@ function vmf.check_wrong_argument_type(mod, vmf_function_name, argument_name, ar
   return true
 end
 
+function vmf.check_old_vmf()
+  local old_vmf_table = rawget(_G, "Mods")
+
+  if old_vmf_table and old_vmf_table.exec then
+    error("Unfortunately, workshop mods and old-fashioned mods (VMF-pack or QoL) are incompatible. " ..
+           "Either remove old mods or disable workshop mods.")
+  end
+end
+
 function vmf.throw_error(error_message, ...)
   error(string.format(error_message, ...), 0)
 end
