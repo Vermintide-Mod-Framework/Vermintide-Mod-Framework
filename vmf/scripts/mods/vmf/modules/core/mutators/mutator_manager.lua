5ce6772f29272b81d5ca04ddfbef7a32bbbf4221
Azumgi <4zumgi@gmail.com>
2019-05-22 14:32:40 +0300

    [Mutators] Expand mutator data validation

--- vmf/scripts/mods/vmf/modules/core/mutators/mutator_manager.lua
+++ vmf/scripts/mods/vmf/modules/core/mutators/mutator_manager.lua
@@ -23,8 +23,14 @@ local ERRORS = {
     required_on_clients_wrong_type = "'required_on_clients' must be a boolean, not %s.",
     enable_before_wrong_type = "'enable_before' must be a table, not %s.",
     enable_before_element_wrong_type = "'enable_before[%s]' must be a string, not %s.",
+    enable_before_contains_self = "Mutators can't put themselves into 'enable_before' table.",
+    enable_before_duplicate_entry = "Detected duplicate mod name inside 'enable_before' table: '%s'.",
     enable_after_wrong_type = "'enable_after' must be a table, not %s.",
     enable_after_element_wrong_type = "'enable_after[%s]' must be a string, not %s.",
+    enable_after_contains_self = "Mutators can't put themselves into 'enable_after' table.",
+    enable_after_duplicate_entry = "Detected duplicate mod name inside 'enable_after' table: '%s'.",
+    enable_after_contains_enable_before_entry = "Detected the same mod name ('%s') inside both 'enable_after' and " ..
+                                                 "'enable_after' tables.",
     dice_wrong_type = "'dice' must be a table, not %s.",
     dice_missing_field = "'dice' table must contain 'bonus', 'tomes', and 'grims' fields.",
     dice_field_wrong_type = "'dice.%s' must be a number, not %s.",
@@ -37,23 +43,16 @@ local ERRORS = {
 -- =============================================================================
 
 -- [THROWS ERRORS]
-local function validate_mutator_data(data)
-  -- Initial check.
+local function validate_mutator_data(mod, data)
   if type(data) ~= "table" then
     vmf.throw_error(ERRORS.THROWABLE.mutator_data_wrong_type, type(data))
   end
 
-  -- Add missing data.
-  if data.required_on_clients == nil then data.required_on_clients = false                             end
-  if data.enable_before       == nil then data.enable_before       = {}                                end
-  if data.enable_after        == nil then data.enable_after        = {}                                end
-  if data.dice                == nil then data.dice                = {bonus = 0, tomes = 0, grims = 0} end
-
-  -- Other checks.
   if type(data.required_on_clients) ~= "boolean" then
     vmf.throw_error(ERRORS.THROWABLE.required_on_clients_wrong_type, type(data.required_on_clients))
   end
 
+  local mutators_enable_before = {}
   if type(data.enable_before) ~= "table" then
     vmf.throw_error(ERRORS.THROWABLE.enable_before_wrong_type, type(data.enable_before))
   end
@@ -61,8 +60,16 @@ local function validate_mutator_data(data)
     if type(mutator_name) ~= "string" then
       vmf.throw_error(ERRORS.THROWABLE.enable_before_element_wrong_type, i, type(mutator_name))
     end
+    if mutator_name == mod:get_name() then
+      vmf.throw_error(ERRORS.THROWABLE.enable_before_contains_self)
+    end
+    if mutators_enable_before[mutator_name] then
+      vmf.throw_error(ERRORS.THROWABLE.enable_before_duplicate_entry, mutator_name)
+    end
+    mutators_enable_before[mutator_name] = true
   end
 
+  local mutators_enable_after = {}
   if type(data.enable_after) ~= "table" then
     vmf.throw_error(ERRORS.THROWABLE.enable_after_wrong_type, type(data.enable_after))
   end
@@ -70,6 +77,16 @@ local function validate_mutator_data(data)
     if type(mutator_name) ~= "string" then
       vmf.throw_error(ERRORS.THROWABLE.enable_after_element_wrong_type, i, type(mutator_name))
     end
+    if mutator_name == mod:get_name() then
+      vmf.throw_error(ERRORS.THROWABLE.enable_after_contains_self)
+    end
+    if mutators_enable_after[mutator_name] then
+      vmf.throw_error(ERRORS.THROWABLE.enable_after_duplicate_entry, mutator_name)
+    end
+    if mutators_enable_before[mutator_name] then
+      vmf.throw_error(ERRORS.THROWABLE.enable_after_contains_enable_before_entry, mutator_name)
+    end
+    mutators_enable_after[mutator_name] = true
   end
 
   if type(data.dice) ~= "table" then
@@ -93,8 +110,19 @@ end
 -- VMF internal functions
 -- =============================================================================
 
-function vmf.initialize_mutator_data(mod, mutator_data)
-  mutator_data = mutator_data or {}
+-- [THROWS ERRORS]
+function vmf.initialize_mutator_data(mod, data)
+  -- Add optional missing fields before validation.
+  data = data or {}
+  if type(data) == "table" then
+    if data.required_on_clients == nil then data.required_on_clients = false                             end
+    if data.enable_before       == nil then data.enable_before       = {}                                end
+    if data.enable_after        == nil then data.enable_after        = {}                                end
+    if data.dice                == nil then data.dice                = {bonus = 0, tomes = 0, grims = 0} end
+  end
+
+  -- [throws errors]
+  validate_mutator_data(mod, data)
 
   validate_mutator_data(mutator_data)
 
