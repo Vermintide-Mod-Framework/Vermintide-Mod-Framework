2384a43b67535df9cc7f9472a3ac94a96e0c3fa5
fracticality <mgjensen11@gmail.com>
2022-06-16 13:56:08 -0600

    custom_hud_components.lua.lua - Component status is reset on `IngameHud.destroy` to ensure components are reinjected properly when party members change.
    toggling.lua
    - Inject/remove events now executed before respective enabled/disabled event hooks.

--- vmf/scripts/mods/vmf/modules/core/toggling.lua
+++ vmf/scripts/mods/vmf/modules/core/toggling.lua
@@ -12,12 +12,12 @@ vmf.set_mod_state = function (mod, is_enabled, initial_call)
 
   if is_enabled then
     mod:enable_all_hooks()
-    vmf.mod_enabled_event(mod, initial_call)
     vmf.inject_hud_components(mod)
+    vmf.mod_enabled_event(mod, initial_call)
   else
     mod:disable_all_hooks()
-    vmf.mod_disabled_event(mod, initial_call)
     vmf.remove_injected_hud_components(mod)
+    vmf.mod_disabled_event(mod, initial_call)
   end
 
   if not (initial_call or mod:get_internal_data("is_mutator")) then
